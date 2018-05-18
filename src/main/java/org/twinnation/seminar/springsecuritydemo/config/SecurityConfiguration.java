15243d7c096ea714eb0c5cc065b3b32c72481dfd
TwinProduction <christian-ct@hotmail.com>
2018-05-18 13:51:00 -0400

    Initial commit

---
+++ src/main/java/org/twinnation/seminar/springsecuritydemo/config/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package org.twinnation.seminar.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableOAuth2Client
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+			.and()
+			.authorizeRequests().anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.and()
+			.oauth2Login();
+	}
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("user").password(passwordEncoder().encode("user")).roles("USER")
+			.and()
+			.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+	}
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
