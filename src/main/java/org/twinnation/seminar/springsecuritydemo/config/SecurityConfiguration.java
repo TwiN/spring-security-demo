1bc20c054a4de6c897b8c5d5e29e149334e3ce3c
TwinProduction <christian-ct@hotmail.com>
2018-05-21 09:37:26 -0400

    added logout button

--- src/main/java/org/twinnation/seminar/springsecuritydemo/config/SecurityConfiguration.java
+++ src/main/java/org/twinnation/seminar/springsecuritydemo/config/SecurityConfiguration.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -21,13 +21,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
-			.and()
+		http
 			.authorizeRequests().anyRequest().authenticated()
 			.and()
-			.formLogin();
-			//.and()
-			//.oauth2Login();
+			.formLogin()
+			.and()
+			.oauth2Login()
+			.and()
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.invalidateHttpSession(true)
+				.clearAuthentication(true)
+				.deleteCookies("JSESSIONID");
 	}
 	
 	
